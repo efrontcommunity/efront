1e3c7d5af660fe321a433f76d41846c18c017939
iuricode <iuricold99@gmail.com>
2023-09-11 00:32:42 -0300

    :lipstick: feat: alterando o design system

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -2,11 +2,11 @@ export function initScrollReveal() {
   const sr = ScrollReveal({
     origin: "top",
     distance: "50px",
-    duration: 2000,
+    duration: 1500,
   });
 
-  sr.reveal(".delaySmallReveal", { delay: 200 });
-  sr.reveal(".delayMediumReveal", { delay: 300 });
-  sr.reveal(".delayLargeReveal", { delay: 400 });
-  sr.reveal(".delayExtraBigReveal", { delay: 600 });
+  sr.reveal(".delaySmallReveal", { delay: 100 });
+  sr.reveal(".delayMediumReveal", { delay: 200 });
+  sr.reveal(".delayLargeReveal", { delay: 300 });
+  sr.reveal(".delayExtraBigReveal", { delay: 500 });
 }
